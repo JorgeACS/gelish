4b1dcf2d69e327ceba45f3b2ae6629152c9b98d0
JorgeACS <jorge_nintendo@hotmail.com>
2017-01-28 20:46:59 -0700

    servidor ya esta puesto

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 "# gelish" 
+
+"# Para instalar dependencias, acceder a la raiz del proyecto desde consola y escribir"
+
+"# npm install"
+
+"# Para correr el servidor, acceder a la raiz del proyecto desde consola y escribir"
+
+"# npm start"
---
+++ server.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var express = require('express');
+var logger = require('morgan');
+var app = express();
+
+// Log the requests
+app.use(logger('dev'));
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for everything else.
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+// Fire it up!
+app.listen(3000);
+console.log('Listening on port 3000');
