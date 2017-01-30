1b0d422cbdd7e9e4fbc97ed1dc73c47d1870c251
JorgeACS <jorge_nintendo@hotmail.com>
2017-01-29 20:47:11 -0700

    ahi esta la base de datos con el usuario

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var path = require('path');
 var express = require('express');
 var logger = require('morgan');
+const mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit: 100,
+  host: "localhost",
+  user: "admin",
+  password: 'gelish123',
+  database: "GELISH"
+});
+
 var app = express();
+
+app.use(function (req,res,next) {
+  req.mysql = pool;
+  next();
+});
 
 // Log the requests
 app.use(logger('dev'));
+
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 // Serve static files
