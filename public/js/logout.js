a5a7c5040945ed6c8e45ab5e89ef20878f22f0c0
JorgeACS <jorge_nintendo@hotmail.com>
2017-02-10 01:31:16 -0700

    intente meter lo del logout

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,15 @@
-var app = angular.module('uiLogout', []);
+var app = angular.module('logout', []);
 app.controller('myCtrl', function($scope, $http) {
 
-    $scope.SalirSesion = function() {
+    $scope.cerrarSesion = function() {
       $http.delete("/logout")
       .success(function(data){
         console.log(data);
+         this.$location.path('/login');
       })
       .error(function(error){
 
       });
-      this.$location.path('/login');
+     
     };
 });
