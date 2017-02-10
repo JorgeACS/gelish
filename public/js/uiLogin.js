65556202fe9f58ef11cf79f3b5c05ea809fd2711
Erick Fdo. Lopez Fimbres <y3rick_18@hotmail.com>
2017-02-09 17:17:34 -0700

    POST con angular

--- public/js/uiLogin.js
+++ public/js/uiLogin.js
@@ -1,14 +1,26 @@
 var app = angular.module('uiLogin', []);
-app.controller('myCtrl', function($scope, $http) {
-
+app.controller('myCtrl', function($scope, $http, $location) {
     $scope.InicioSesion = function() {
-      $http.get("/login")
+      console.log("click");
+      user={
+        username: $scope.username,
+        password:$scope.password
+      };
+      console.log(user.username);
+      console.log(user.password);
+      $http.post('/login',user).then((res) => {
+        if(res.data.tipo==1){
+          $location.path('admin');
+        }
+      })
+    };
+});
+      /*$http.get("http://localhost:3000/login")
       .success(function(data){
-        console.log(data);
+        console.log("Va");
       })
       .error(function(error){
 
       });
       this.$location.path('/admin');
-    };
-});
+    };*/
