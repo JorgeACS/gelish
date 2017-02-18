500d577c26046a2ac3d144956eddffc6c04cc540
JorgeACS <jorge_nintendo@hotmail.com>
2017-02-17 22:55:11 -0700

    insertando views de productos y servicios, lista la bd

--- bd.sql
+++ bd.sql
@@ -4,6 +4,14 @@ CREATE DATABASE GELISH;
 
 USE GELISH;
 
+CREATE TABLE Sucursal(
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  plaza VARCHAR(50) NOT NULL,
+  ciudad VARCHAR(20) NOT NULL,
+  telefono VARCHAR(20) NOT NULL
+);
+
+
 CREATE TABLE Usuario(
   id INT(11) PRIMARY KEY AUTO_INCREMENT,
   sucursal_id INT(11) NOT NULL,
@@ -28,27 +36,12 @@ CREATE TABLE Tecnica(
     (usuario_id) REFERENCES Usuario(id)
 );
 
-CREATE TABLE Sucursal(
-  id INT(11) PRIMARY KEY AUTO_INCREMENT,
-  plaza VARCHAR(50) NOT NULL,
-  ciudad VARCHAR(20) NOT NULL,
-  telefono VARCHAR(20) NOT NULL
-);
 
 CREATE TABLE Servicios(
   id INT(11) PRIMARY KEY AUTO_INCREMENT,
   nombre VARCHAR(50) NOT NULL,
   descripcion VARCHAR(255) NOT NULL,
-  precio INT(11) NOT NULL,
-);
-CREATE TABLE Productos(
-  id INT(11) PRIMARY KEY AUTO_INCREMENT,
-  categoria_id TINYINT(11) NOT NULL,
-  nombre VARCHAR(50) NOT NULL,
-  descripcion VARCHAR(255) NOT NULL,
-  precio INT(11) NOT NULL,
-  CONSTRAINT FOREIGN KEY
-    (categoria_id) REFERENCES Categoria(id)
+  precio INT(11) NOT NULL
 );
 
 CREATE TABLE Categoria(
@@ -56,12 +49,49 @@ CREATE TABLE Categoria(
   nombre VARCHAR(200) NOT NULL
 );
 
-CREATE TABLE Nota(
+CREATE TABLE Productos(
   id INT(11) PRIMARY KEY AUTO_INCREMENT,
-  fecha INT(11) NOT NULL,
-  
+  categoria_id INT(11) NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  descripcion VARCHAR(255) NOT NULL,
+  precio INT(11) NOT NULL,
+  CONSTRAINT FOREIGN KEY
+    (categoria_id) REFERENCES Categoria(id)
 );
 
+
+CREATE TABLE Nota(
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  usuario_id INT(11) NOT NULL, 
+  fecha INT(11) NOT NULL,
+  total INT(11) NOT NULL,
+  CONSTRAINT FOREIGN KEY
+    (usuario_id) REFERENCES Usuario(id)
+);
+
+CREATE TABLE Caja(
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  recepcionista_id INT(11) NOT NULL,
+  fecha_apertura DATE NOT NULL,
+  fecha_cierre DATE,
+  CONSTRAINT FOREIGN KEY
+    (recepcionista_id) REFERENCES Usuario(id)
+);
+
+CREATE TABLE Venta_productos_Servicios(
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  tecnica_id INT(11),
+  nota_id INT(11) NOT NULL,
+  tipo INT(11) NOT NULL,
+  precio INT(11) NOT NULL,
+  CONSTRAINT FOREIGN KEY
+    (tecnica_id) REFERENCES Usuario(id),
+  CONSTRAINT FOREIGN KEY
+    (nota_id) REFERENCES Nota(id)
+);
+
+INSERT INTO Sucursal(plaza,ciudad,telefono)
+  VALUES("DILA","HERMOSILLO","6622012345");
 
 INSERT INTO Usuario(sucursal_id,username,password,nombre,apellido,correo,telefono,tipo)
 	VALUES(1,"admin","admin","Juan Pablo","Soto","jpsoto@gelish.com","6621010203",0);
--- public/js/uiLogin.js
+++ public/js/uiLogin.js
@@ -9,8 +9,32 @@ app.controller('myCtrl', function($scope, $http, $location,$window,$rootScope) {
         password:$scope.password
       };
       $http.post('/login',user).then((res) => {
+        //console.log(res.data.nombre)
+        
+        if(res.data.tipo==0){
+
+          $window.location.href ="/admin";
+        }else if(res.data.tipo==1){
+          $window.location.href ="/adminSuc";
+        }else{
+          $window.location.href ="/recepcionista";
+        }
+      })
+    };
+    
+});
+
+app.controller('user', function($scope, $http, $location,$window,$rootScope) {
+    $scope.InicioSesion = function() {
+      user={
+        username: $scope.username,
+        password:$scope.password
+      };
+      $http.post('/login',user).then((res) => {
 
         if(res.data.tipo==0){
+
+
           $window.location.href ="/admin";
         }else if(res.data.tipo==1){
           $window.location.href ="/adminSuc";
@@ -20,3 +44,5 @@ app.controller('myCtrl', function($scope, $http, $location,$window,$rootScope) {
       })
     };
 });
+
+ 
