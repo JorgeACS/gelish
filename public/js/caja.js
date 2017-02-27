8ac23ef2b3d365316a2737c72c6377b2da209206
Erick Fdo. Lopez Fimbres <y3rick_18@hotmail.com>
2017-02-26 22:36:18 -0700

    Detalles a recepcionista y ahora al abrir caja cambia el candado a abierto

--- public/js/caja.js
+++ public/js/caja.js
@@ -1,16 +1,17 @@
 function toggleCaja(){
-	
+
 	if (document.getElementById("estadoCaja").innerHTML == "La caja esta cerrada"){//cerrar la caja
 		document.getElementById("estadoCaja").innerHTML = "La caja esta abierta";
 		document.getElementById("botonCaja").innerHTML = "Cerrar Caja"
 		var lista = document.getElementById("notaVenta").style;
 		lista.display = "none";
+		document.getElementById("cajaI").className="fa fa-lock";
 	}else {//abrir caja
 		document.getElementById("estadoCaja").innerHTML = "La caja esta cerrada"
 		document.getElementById("botonCaja").innerHTML = "Abrir Caja"
 		var lista = document.getElementById("notaVenta").style;
 		lista.display = "block";
-
+		document.getElementById("cajaI").className="fa fa-unlock";
 	}
 
 }
